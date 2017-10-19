6350a95eada5b42fbc222cdb3e13306e6476ef32
Jack Wu( [♠]]] ) <xuwupeng2000@gmail.com>
2017-10-20 07:19:10 +1300

    Finish up react webpack setup

--- app/assets/javascripts/components/app.jsx
+++
@@ -1,7 +0,0 @@
-var App = React.createClass({
-
-  render() {
-    return <h1>Hello from {this.props.name}!</h1>;
-  }
-
-});
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   entry: './client/index.js',
   output: {
-    path: path.resolve('dist'),
+    path: path.resolve('./public'),
     filename: 'bundle.js'
   },
   module: {
