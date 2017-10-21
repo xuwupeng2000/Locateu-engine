0bfb4fa6faa6698b45f72a809dc147b0817aef3c
Jack Wu( [♠]]] ) <xuwupeng2000@gmail.com>
2017-10-21 22:09:57 +1300

    PenTo the most recent POI

--- client/components/gmap.jsx
+++ client/components/gmap.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 import { httpClient } from './http_client.js'
+import _ from 'lodash' 
 
 class PoiLitItem extends Component {
 
@@ -12,7 +13,7 @@ class PoiLitItem extends Component {
         <td>{track.created_at}</td>
         <td>{track.user.username}</td>
         <td>{track.sensor.serial_code}</td>
-        <td> <button className="button button-small button-outline">View</button> </td>
+        <td> <button onClick={this.props.onClick} className="button button-small button-outline">View</button> </td>
       </tr>
     );
   }
@@ -32,7 +33,9 @@ class Gmap extends Component {
     httpClient.get('/api/v1/user_geo_tracks')
       .then((resp) => {
         let tracks = resp.data.geo_tracks;
-        this.setState({ geo_tracks: tracks });
+        let mostRecent = _.last(tracks);
+        this.setState({ geo_tracks: tracks});
+        this.map.panTo({lat: mostRecent.lat, lng: mostRecent.lng});
       })
       .catch((err) => {
         alert(err);
@@ -44,22 +47,24 @@ class Gmap extends Component {
   }
 
   render() {
-    let showTracks = this.state.geo_tracks.map((track) => {
+    let showTracks = this.state.geo_tracks.map((track, index) => {
       return (
-        <Marker key={track.id} position={{lat: track.lat, lng: track.lng}} />
+        <Marker key={track.id} label={index.toString()} position={{lat: track.lat, lng: track.lng}} />
       );
     });
 
     let list = this.state.geo_tracks.map((track) => {
       return (
-        <PoiLitItem key={track.id} track={track}></PoiLitItem>
+        <PoiLitItem key={track.id} track={track} onClick={this.onClick}></PoiLitItem>
       );
     });
 
     return (
       <div>
-        <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }} >
-          <Marker position={{ lat: -34.397, lng: 150.644 }} />
+        <GoogleMap 
+          ref={(map) => { this.map = map; }}
+          defaultZoom={8} 
+          defaultCenter={{ lat: -34.397, lng: 150.644 }} >
           {showTracks}
         </GoogleMap>
 
