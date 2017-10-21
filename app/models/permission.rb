622b1d51ce5377d8dde5a4a84c1bdcffc3e600e9
Jack Wu( [♠]]] ) <xuwupeng2000@gmail.com>
2017-10-22 12:06:17 +1300

    Fix typo, blongs to has no source

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -5,7 +5,6 @@ class Permission < ApplicationRecord
   include Statesman::Adapters::ActiveRecordQueries
 
   belongs_to :user
-  belongs_to :ec, source: :ec, class: 'User'
   has_many :permission_transitions, autosave: false
 
   def state_machine
