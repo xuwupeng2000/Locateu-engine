44a8856d2ba80b51cb4f22b53c246096e0864b25
Jack Wu( [♠]]] ) <xuwupeng2000@gmail.com>
2017-10-29 23:17:22 +1300

    Add new factory, add sensor specific index action

--- app/controllers/api/v1/user_geo_tracks_controller.rb
+++ app/controllers/api/v1/user_geo_tracks_controller.rb
@@ -6,7 +6,12 @@ class Api::V1::UserGeoTracksController < ApplicationController
   # Server don't know the user profile, we can only tell where each sensor have been
   # Where our Map UI data comes from
   def index
-    @geo_tracks = current_user.geo_tracks.order(created_at: :desc)
+    @geo_tracks = if params[:serial_code]
+                    sensor = current_user.sensors.find_by(serial_code: params[:serial_code])
+                    current_user.geo_tracks.where(sensor: sensor).order(created_at: :desc)
+                  else
+                    current_user.geo_tracks.order(created_at: :desc)
+                  end
     @sensors = current_user.sensors
   end
 
--- spec/factories/geo_tracks.rb
+++ spec/factories/geo_tracks.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :geo_track do
     sensor
-    lnglat {"Point(#{Faker::Address.longitude} #{Faker::Address.latitude})"}
+    lnglat {
+      lat = rand(36.848461...36.948461)
+      lng = rand(174.763336...174.963336)
+      "Point(#{lng} -#{lat})"}
   end
 end
